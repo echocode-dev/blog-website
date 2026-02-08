02d5768b77b5f6a72a6dbe526848faf9bb10f5fb
Cristóbal Veas <cristobal.veas@proton.me>
2026-02-08 00:22:19 -0300

    feat: add more metadata to pages to improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import sectionize from "@hbsnow/rehype-sectionize";
 
 export default defineConfig({
+  site: "https://www.echocode.dev",
   markdown: {
     rehypePlugins: [sectionize],
   },
