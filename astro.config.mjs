5233b620cf34a3f06c42d0a571ea7ceb036c9207
Cristóbal Veas <cristobal.veas@proton.me>
2026-02-17 01:57:22 -0300

    build: add pagefind and new scripts to manage it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import sectionize from "@hbsnow/rehype-sectionize";
+import pagefind from "astro-pagefind";
 
 export default defineConfig({
   site: "https://www.echocode.dev",
+  integrations: [pagefind()],
   markdown: {
     rehypePlugins: [sectionize],
   },
