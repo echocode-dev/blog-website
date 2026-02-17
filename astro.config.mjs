33057752345d3a3bc9fda33d58ce7c2ef934a1ff
Cristóbal Veas <cristobal.veas@proton.me>
2026-02-17 12:44:51 -0300

    feat: add section header anchors and smooth scrolling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,32 @@
 import { defineConfig } from "astro/config";
 import sectionize from "@hbsnow/rehype-sectionize";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import pagefind from "astro-pagefind";
 
 export default defineConfig({
   site: "https://www.echocode.dev",
   integrations: [pagefind()],
   markdown: {
-    rehypePlugins: [sectionize],
+    rehypePlugins: [
+      rehypeHeadingIds,
+      sectionize,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          content: {
+            type: "text",
+            value: "#",
+          },
+          headingProperties: {
+            className: ["anchor"],
+          },
+          properties: {
+            className: ["anchor-link"],
+          },
+        },
+      ],
+    ],
   },
 });
