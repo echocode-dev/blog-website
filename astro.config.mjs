79aeb7c5f8afb6430f60cc502eed35d0bd20ca91
Cristóbal Veas <cristobal.veas@proton.me>
2026-03-02 00:39:26 -0300

    feat(i18n): localize sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,7 +36,15 @@ export default defineConfig({
         },
       },
     }),
-    sitemap(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          es: "es",
+        },
+      },
+    }),
     mdx(),
   ],
   markdown: {
