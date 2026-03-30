4483ecf6e3d4a955f03feb1fcadc0dcca43e306e
Cristóbal Veas <cristobal.veas@proton.me>
2026-03-29 21:07:36 -0300

    build: disable @playform/compress temporaly
    Take too much to build on our poor free tier instance :(

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import pagefind from "astro-pagefind";
 import expressiveCode from "astro-expressive-code";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import playformCompress from "@playform/compress";
+// import playformCompress from "@playform/compress";
 import { LOCALES, defaultLang } from "./src/i18n/ui";
 
 export default defineConfig({
@@ -45,7 +45,7 @@ export default defineConfig({
     }),
     sitemap(),
     mdx(),
-    playformCompress(),
+    //playformCompress(),
   ], // IMPORTANT: playformCompress should always be last!
   markdown: {
     rehypePlugins: [
